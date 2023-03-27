2016ba75d971d5d98c3de8bc32f98133c223ee3f
Dávid Pálfy <palfydavid9601@gmail.com>
2023-03-27 20:08:54 +0200

    Default products will be inserted into the database when application starts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (name, price) VALUES ('apple', 0.4);
+INSERT INTO product (name, price) VALUES ('tomato', 0.2);
+INSERT INTO product (name, price) VALUES ('banana', 0.3);
+INSERT INTO product (name, price) VALUES ('avocado', 1.0);
